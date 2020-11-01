1365736fd5ddfbad332892bc9cbb05ad8ad1685a
Artem <artem.fx4y@gmail.com>
2020-11-01 23:59:37 +0300

    #2 HomeWork for lesson 2

---
+++ vkClient/vkClient/Controllers/Friends/Cells/FriendCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  FriendCollectionViewCell.swift
+//  vkClient
+//
+//  Created by MacBook Pro on 01.11.2020.
+//
+
+import UIKit
+
+class FriendCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ vkClient/vkClient/Controllers/Login/LoginFormController.swift
@@ -0,0 +1,65 @@
+//
+//  LoginFormController.swift
+//  vkClient
+//
+//  Created by MacBook Pro on 25.10.2020.
+//
+
+import UIKit
+
+class LoginFormController: UIViewController {
+    
+    // MARK: - IBOutlets
+
+    @IBOutlet var loginTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var loginButton: UIButton!
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+        // TODO: Разобраться и сделать логику с появлением и скрытием клавиатуры.
+    }
+
+    // MARK: - Private Methods
+
+    private func showMessage(message: String) {
+        let alert = UIAlertController(title: "Ошибка", message: message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        let login = loginTextField.text!
+        let password = passwordTextField.text!
+        guard login == "1" && password == "1" else {
+            showMessage(message: "Не верный логин или пароль")
+            return false
+        }
+        return true
+    }
+
+    
+    // MARK: - IBActions
+
+
+    @IBAction func LlogOut(unwindSegue: UIStoryboardSegue) {
+
+    }
+
+    @IBAction func TextField_EditingChanged(_ sender: UITextField) {
+        if loginTextField.text != "" && passwordTextField.text != "" {
+            loginButton.isEnabled = true
+            loginButton.alpha = 1.0
+
+        } else {
+            loginButton.isEnabled = false
+            loginButton.alpha = 0.6
+        }
+    }
+}
+
+
