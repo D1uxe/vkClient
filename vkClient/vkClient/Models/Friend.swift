498f5034f3691d09881d8067224e16d61fc3e0e6
Artem <artem.fx4y@gmail.com>
2020-11-05 21:12:26 +0300

    #3.2 Customized collection cells to align photos

--- vkClient/vkClient/Models/Friend.swift
+++ vkClient/vkClient/Models/Friend.swift
@@ -16,7 +16,7 @@ struct Friend {
 struct Friends {
     var friendsList: [Friend] = [
         Friend(name: "Натали Романов", avatar: "BlackWidow", photo: ["BlackWidow1", "BlackWidow2"]),
-        Friend(name: "Тони Iron Man Старк", avatar: "IronMan", photo: ["IronMan1", "IronMan2", "IronMan3", "IronMan4", "IronMan5", "IronMan6"]),
+        Friend(name: "Тони Iron Man Старк", avatar: "IronMan", photo: ["IronMan1", "IronMan2", "IronMan3", "IronMan4", "IronMan5", "IronMan6","IronMan1","IronMan3"]),
         Friend(name: "Брюс Hulk Беннер", avatar: "Hulk", photo: ["Hulk1", "Hulk2"]),
         Friend(name: "Wonder Woman", avatar: "WonderWoman", photo: ["WonderWoman1", "WonderWoman2"]),
         Friend(name: "Tanos", avatar: "Tanos", photo: ["Tanos1", "Tanos2", "Tanos3"])
